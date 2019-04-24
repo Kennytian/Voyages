c3fb5dde1ca4bcc2ea5b7064c954b7601f6519ab
Kennytian <kt.tian@gmail.com>
2019-04-24 23:23:22 +0800

    Add autotest for iPhone platform

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { Platform, StyleSheet, Text, TouchableOpacity, View, TextInput } from 'react-native';
 import * as RNLocalize from "react-native-localize";
 
 const instructions = Platform.select({
@@ -10,28 +10,60 @@ const instructions = Platform.select({
 });
 
 interface Props {}
-export default class App extends Component<Props> {
-  componentDidMount() {
 
-  }
+interface State {
+  text: string;
+}
+
+export default class App extends Component<Props, State> {
+  state = {
+    text: "",
+  };
 
   render() {
     console.log(RNLocalize.getLocales());
     console.log(RNLocalize.getCurrencies());
+    const { text } = this.state;
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text testID={'welcome'} style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.tsx</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <TouchableOpacity testID='MyUniqueId123'>
+          <Text>Some button</Text>
+        </TouchableOpacity>
+        <TouchableOpacity testID='hello_button'>
+          <Text>Hello!!!</Text>
+        </TouchableOpacity>
+        <TouchableOpacity testID='world_button'>
+          <Text>World!!!</Text>
+        </TouchableOpacity>
+        <Text>Type some stuff below</Text>
+        <TextInput
+          testID="textInput"
+          value={text}
+          autoCapitalize="none"
+          style={styles.textInput}
+          onChangeText={(text) => this.setState({ text })}
+        />
+        <Text>The above but reversed: {"\n"}</Text>
+        <Text testID="reversedText" style={styles.reversedText}>
+          {this.reverseText(text)}
+        </Text>
       </View>
     );
   }
+
+  private reverseText = (text: string): string => {
+    return text.split("").reverse().join("");
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    // justifyContent: 'center',
+    marginTop: 50,
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
@@ -43,6 +75,19 @@ const styles = StyleSheet.create({
   instructions: {
     textAlign: 'center',
     color: '#333333',
+    marginBottom: 5,
+  },
+  textInput: {
+    width: "50%",
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
+  reversedText: {
+    textAlign: "center",
+    color: "#333333",
     marginBottom: 5,
   },
 });
---
+++ e2e/firstTest.spec.ts
@@ -0,0 +1,43 @@
+import { expect, device, element, by } from "detox";
+
+describe('Example', () => {
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('should have welcome screen', async () => {
+    await expect(element(by.id('welcome'))).toBeVisible();
+  });
+
+  it('should show myButton screen after tap', async () => {
+    await element(by.id('MyUniqueId123')).tap();
+  });
+
+  it('should show hello screen after tap', async () => {
+    await element(by.id('hello_button')).tap();
+    await expect(element(by.text('Hello!!!'))).toBeVisible();
+  });
+
+  it('should show world screen after tap', async () => {
+    await element(by.id('world_button')).tap();
+    await expect(element(by.text('World!!!'))).toBeVisible();
+  });
+
+
+  it("should reverse text in text input", async () => {
+    const input = "abcdefghijklmnopqrstuvwxyz";
+    await element(by.id("textInput")).tap();
+    await element(by.id("textInput")).typeText(input);
+    await expect(element(by.id("reversedText"))).toHaveText(
+      reverse("abcdefghijklmnopqrstuvwxyz"),
+    );
+  });
+});
+
+// Verify that we can use TypeScript constructs
+function reverse(text?: string) {
+  if (text) {
+    return text.split("").reverse().join("");
+  }
+  return "";
+}

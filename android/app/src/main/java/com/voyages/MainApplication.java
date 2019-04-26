cea22cbca312bccfe7ce63175aa956db3de71e5a
Kenny <kenny@Kennys-Mac-mini.local>
2019-04-26 09:14:12 +0800

    Add device info dependency

--- android/app/src/main/java/com/voyages/MainApplication.java
+++ android/app/src/main/java/com/voyages/MainApplication.java
@@ -12,6 +12,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import com.reactcommunity.rnlocalize.RNLocalizePackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -25,7 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-          new RNLocalizePackage()
+          new RNLocalizePackage(),
+          new RNDeviceInfo()
       );
     }
 

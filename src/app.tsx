cfdfd30037f2fc2c3fcae4922914251b87709b77
Kennytian <kt.tian@gmail.com>
2019-04-29 23:26:52 +0800

    Add navigation features and e2e code

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import HomeScreen from './home';
+import DetailScreen from './detail';
 
-const AppNavigator = createStackNavigator({
-  Home: { screen: HomeScreen },
-});
+const AppNavigator = createStackNavigator(
+  {
+    Home: { screen: HomeScreen },
+    Detail: { screen: DetailScreen },
+  },
+  {
+    initialRouteName: 'Home',
+  },
+);
 
 export default createAppContainer(AppNavigator);
